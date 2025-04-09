c71de9a7622c6b1417df20b652cc950d85c86cdf
Nanahawaw <folarinyetunde@gmail.com>
2025-04-09 08:00:28 -0700

    setting up

---
+++ src/main/java/com/nans/nans_school/NansSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.nans.nans_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NansSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NansSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nans/nans_school/NansSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nans.nans_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NansSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
